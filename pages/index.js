7467cb8b00a4ef199465a97abe286d4b4a12d70c
Zoie <greenmoonshisu@gmail.com>
2022-10-01 14:29:11 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import "@rainbow-me/rainbowkit/styles.css";
 import {
   ConnectButton,
   getDefaultWallets,
-  RainbowKitProvider
+  RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
@@ -16,13 +17,13 @@ const { chains, provider } = configureChains(
 
 const { connectors } = getDefaultWallets({
   appName: "My RainbowKit App",
-  chains
+  chains,
 });
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
+  provider,
 });
 
 export default function IndexPage() {
@@ -35,13 +36,17 @@ export default function IndexPage() {
               width: "100vw",
               height: "100vh",
               display: "flex",
+              flexDirection: "column",
               alignItems: "center",
               justifyContent: "center",
-              border: "solid",
-              borderWidth: "1px"
+              border: "dot",
+              borderWidth: "2px",
             }}
           >
             <ConnectButton />
+            <Box bg="tomato" w="100%" p={4} color="white" flex={1}>
+              This is the Box
+            </Box>
           </div>
         </ChakraProvider>
       </RainbowKitProvider>
